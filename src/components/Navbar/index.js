03538f5325c7fce27ba62ff1f11c36d61cf84159
flooff <flo133.dillenschneider@gmail.com>
2021-02-21 12:13:05 +0100

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {FaBars} from 'react-icons/fa'
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink
+} from './NavbarElement';
+
+const Navbar= () => {
+    return (
+        <>
+          <Nav>
+            <NavbarContainer>
+                <NavLogo to='/'>FlowBank</NavLogo>
+                <MobileIcon>
+                  <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                  <NavItem>
+                    <NavLinks to="about">About</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to="Discover">Discover</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to="Services">Services</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to="Signup">Sign Up</NavLinks>
+                  </NavItem>
+                </NavMenu>
+                <NavBtn>
+                <NavBtnLink to="/signin">Sign in</NavBtnLink>
+                </NavBtn>
+            </NavbarContainer>
+          </Nav>  
+        </>
+    )
+}
+
+export default Navbar;
