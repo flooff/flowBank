e8a39b84b13f4478d58e296a5f8b7c71ef8c091d
flooff <flo133.dillenschneider@gmail.com>
2021-02-22 15:54:29 +0100

    heroRefactor

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -12,13 +12,13 @@ import {
   NavBtnLink
 } from './NavbarElement';
 
-const Navbar= () => {
+const Navbar= ( {toggle}) => {
     return (
         <>
           <Nav>
             <NavbarContainer>
                 <NavLogo to='/'>FlowBank</NavLogo>
-                <MobileIcon>
+                <MobileIcon onClick={toggle}>
                   <FaBars />
                 </MobileIcon>
                 <NavMenu>
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarWrapper,
+  SidebarMenu,
+  SideBtnWrap,
+  SidebarRoute,
+  SidebarLink
+} from './SidebarElement'
+
+const Sidebar = ({isOpen, toggle}) => {
+    return (
+        <SidebarContainer isOpen={isOpen} onClick={toggle}>
+            <Icon onClick={toggle}>
+                <CloseIcon>
+                    
+                </CloseIcon>
+            </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to="about" onClick={toggle}>
+                        about
+                    </SidebarLink>
+                    <SidebarLink to="discover" onClick={toggle}>
+                        Discover
+                    </SidebarLink>
+                    <SidebarLink to="services" onClick={toggle}>  
+                        Services
+                    </SidebarLink>
+                    <SidebarLink to="signup" onClick={toggle}>
+                        signup
+                    </SidebarLink>
+                </SidebarMenu>
+                <SideBtnWrap>
+                    <SidebarRoute to="/sign in">Sign In</SidebarRoute>
+                </SideBtnWrap>
+            </SidebarWrapper>
+        </SidebarContainer>
+    )
+}
+
+export default Sidebar;
